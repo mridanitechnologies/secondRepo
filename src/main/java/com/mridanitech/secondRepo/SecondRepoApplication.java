abc0a65aabb52a9446b98c163b83c53aecf15c3f
mridanitechno <47944427+mridanitechno@users.noreply.github.com>
2020-02-26 22:48:29 +0530

    initial checkins

---
+++ src/main/java/com/mridanitech/secondRepo/SecondRepoApplication.java
@@ -0,0 +1,13 @@
+package com.mridanitech.secondRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mridanitech/secondRepo/SecondRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mridanitech.secondRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
